5b1392ef7b2b8de030243f213ad5f4ebaf73e112
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-04-25 15:16:46 +0400

    add: unordered lists for drop down menus

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,9 +4,10 @@ import React from "react";
 import Image from "next/image";
 import { memo } from "react";
 
-import { NavbarProps } from "@/types/type";
+import { ActiveElement, NavbarProps } from "@/types/type";
 
 import ActiveUsers from "./users/ActiveUsers";
+import { navElements } from "@/constants";
 
 const Navbar = ({ activeElement }: NavbarProps) => {
   return (
@@ -16,6 +17,20 @@ const Navbar = ({ activeElement }: NavbarProps) => {
   bg-primary-black px-5 text-white"
     >
       <Image src="/assets/logo.svg" alt="FigPro Logo" width={58} height={20} />
+
+      <ul className="flex flex-row">
+        {navElements.map((item: ActiveElement | any) => (
+          <li key={item.name}>
+            {Array.isArray(item.value) ? (
+              <ShapesMenu />
+            ) : item?.value === "comments" ? (
+              <NewThread></NewThread>
+            ) : (
+              <button></button>
+            )}
+          </li>
+        ))}
+      </ul>
       <ActiveUsers />
     </nav>
   );
